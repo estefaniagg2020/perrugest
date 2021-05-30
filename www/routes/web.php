ddbfdb1105072a6f643f1f9c8a502de4de35ca07
estefania <egarcia@roi-up.es>
2021-05-30 18:49:18 +0200

    arreglado proyecto

--- www/routes/web.php
+++ www/routes/web.php
@@ -1,6 +1,7 @@
 
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::apiResource('/costumers', App\Http\Controllers\CostumerController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
